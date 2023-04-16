2ebb57eec9d3ae7ed7ebcd0eb67ddacfff23f64f
guna72 <10628714@mydbs.ie>
2023-04-16 22:51:40 +0100

    second commit

--- Billing Information system/www.deyhardware.com/application/views/Admin/admin_footer.php
+++
@@ -1,9 +0,0 @@
-<!-- For Bootstrap CDN for JQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- For Bootstrap CDN Javascript -->
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <!-- <div class="footer">
- 	Copyright@<?php echo date('Y'); ?> | Design By <a href="http://webtesla.com" target="_blank">WebTesla.Com</a>
- </div> -->
-</body>
-</html>
--- Billing Information system/www.deyhardware.com/application/views_OLD/Admin/page_not_found.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-        <h4>Page Not Found!!</h4>
-</body>
-</html>
